a8e95c28dbbace2ef911619f8594cb2b8d452dca
omarovici <omarkhalidsaber042004@gmail.com>
2025-07-05 08:59:51 +0300

    Add ShippedAndExpiredProducts class

---
+++ Fawry Rise Journey Challenge/Models/ShippedAndExpiredProducts.cs
@@ -0,0 +1,14 @@
+using Fawry_Rise_Journey_Challenge.Interfaces;
+
+namespace Fawry_Rise_Journey_Challenge.Models;
+
+public class ShippedAndExpiredProducts(string name, int price, int quantity, DateTime expired, int weight) : ExpiredProduct(name, price, quantity, expired),IShipping
+{
+    public int Weight  { get; set; } = weight;
+
+    public string GetName() => Name;
+
+    public int GetWeight()  => Weight;
+
+    public override bool IsShipping() => true;
+}
